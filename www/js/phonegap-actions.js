c4933dbea68c9b369652045fb5014d62a8d45604
ricardo-lemes <ricardolemes@globo.com>
2018-10-30 19:13:33 -0200

    imagens ipad tablet

--- www/js/phonegap-actions.js
+++ www/js/phonegap-actions.js
@@ -1,7 +1,18 @@
 document.addEventListener("deviceready", onDeviceReady, false);
+var tablet;
 function onDeviceReady() {
-    setScreenOrientation('portrait')
-    StatusBar.hide()
+    var deviceType = (navigator.userAgent.match(/iPad/i)) == "iPad" ? "iPad" : (navigator.userAgent.match(/iPhone/i)) == "iPhone" ? "iPhone" : (navigator.userAgent.match(/Android/i)) == "Android" ? "Android" : (navigator.userAgent.match(/BlackBerry/i)) == "BlackBerry" ? "BlackBerry" : "null";
+    var w, h;
+    w = window.innerWidth
+    h = window.innerHeight
+
+    tablet = deviceType == "iPad" ? true : (w > 736 || h > 736) ? true : false
+    if (tablet) {
+        setScreenOrientation('landscape')
+    } else {
+        setScreenOrientation('portrait')
+        StatusBar.hide()
+    }
 }
 var setScreenOrientation = function (position, callback) {
     //alert('aqui')
