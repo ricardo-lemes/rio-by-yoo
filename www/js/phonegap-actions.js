68cff53e2d0697d204a1f840228e35b91ac4f975
ricardo-lemes <ricardolemes@globo.com>
2018-11-01 18:05:52 -0200

    ipad - ok

--- www/js/phonegap-actions.js
+++ www/js/phonegap-actions.js
@@ -1,12 +1,7 @@
 document.addEventListener("deviceready", onDeviceReady, false);
-var tablet;
+// var tablet;
 function onDeviceReady() {
-    var deviceType = (navigator.userAgent.match(/iPad/i)) == "iPad" ? "iPad" : (navigator.userAgent.match(/iPhone/i)) == "iPhone" ? "iPhone" : (navigator.userAgent.match(/Android/i)) == "Android" ? "Android" : (navigator.userAgent.match(/BlackBerry/i)) == "BlackBerry" ? "BlackBerry" : "null";
-    var w, h;
-    w = window.innerWidth
-    h = window.innerHeight
 
-    tablet = deviceType == "iPad" ? true : (w > 736 || h > 736) ? true : false
     if (tablet) {
         setScreenOrientation('landscape')
     } else {
